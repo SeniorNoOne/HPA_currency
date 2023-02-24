a824d39896a66a25d0cec0cc6126f99f94513f02
Vadym Martyniuk <MartynyukVadum@gmail.com>
2023-02-24 18:46:16 +0200

    Minor changes in views.py Fixed file extention for currency.html Added some Makefile commands

--- app/currency/views.py
+++ app/currency/views.py
@@ -1,17 +1,16 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 from currency.models import Rate, ContactUs
 
 
 def show_currency_lst(request):
-    result = [str(querry) for querry in Rate.objects.all()]
-    if not result:
-        result = ["No records in CURRENCY_RATE database"]
-    return HttpResponse("<br>".join(result))
+    result = Rate.objects.all()
+    context = {'currency_lst': result}
+    return render(request, 'currency.html', context)
 
 
 def show_email_lst(request):
-    result = [str(querry) for querry in ContactUs.objects.all()]
-    if not result:
-        result = ["No records in CURRENCY_CONTACTUS database"]
-    return HttpResponse("<br>".join(result))
+    result = ContactUs.objects.all()
+    print(result)
+    context = {'user_data': result}
+    return render(request, 'contact_us.html', context)
