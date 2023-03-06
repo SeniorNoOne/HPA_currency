87f33cc06812128eeeb8873c5a69682723bfc815
Martyniuk Vadym <martynyukvadum@gmail.com>
2023-03-06 22:54:02 +0200

    Reworked all links
    Added urls.py for currency app as well as app_name for these links
    Added names for all currency app links
    Changed all links in currency app templates

--- app/currency/views.py
+++ app/currency/views.py
@@ -1,100 +1,100 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView, DeleteView, CreateView, UpdateView
-from currency.forms import RateForm, ContactUsForm, SourceForm
-from currency.models import Rate, ContactUs, Source
+from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView, DeleteView, DetailView, ListView, UpdateView, TemplateView
+)
+from currency.forms import ContactUsForm, RateForm, SourceForm
+from currency.models import ContactUs, Rate, Source
 
 
-# main page
-def index(request):
-    context = {}
-    return render(request, 'index.html', context)
+class MainPageView(TemplateView):
+    template_name = 'index.html'
 
 
 class RateCreateView(CreateView):
     form_class = RateForm
-    template_name = "rate/rate_create.html"
+    template_name = 'rate/rate_create.html'
     queryset = Rate.objects.all()
-    success_url = "/rate/list/"
+    success_url = reverse_lazy('currency:rate-list')
 
 
 class RateListView(ListView):
-    template_name = "rate/rate_list.html"
+    template_name = 'rate/rate_list.html'
     queryset = Rate.objects.all()
 
 
 class RateDetailView(DetailView):
-    template_name = "rate/rate_details.html"
+    template_name = 'rate/rate_details.html'
     queryset = Rate.objects.all()
 
 
 class RateUpdateView(UpdateView):
     form_class = RateForm
-    template_name = "rate/rate_update.html"
+    template_name = 'rate/rate_update.html'
     queryset = Rate.objects.all()
-    success_url = "/rate/list/"
+    success_url = reverse_lazy('currency:rate-list')
 
 
 class RateDeleteView(DeleteView):
-    template_name = "rate/rate_delete.html"
+    template_name = 'rate/rate_delete.html'
     queryset = Rate.objects.all()
-    success_url = "/rate/list/"
+    success_url = reverse_lazy('currency:rate-list')
 
 
 class ContactUsCreateView(CreateView):
     form_class = ContactUsForm
-    template_name = "contact_us/contact_us_create.html"
+    template_name = 'contact_us/contact_us_create.html'
     queryset = ContactUs.objects.all()
-    success_url = "/contact_us/list/"
+    success_url = reverse_lazy('currency:contactus-list')
 
 
 class ContactUsListView(ListView):
-    template_name = "contact_us/contact_us_list.html"
+    template_name = 'contact_us/contact_us_list.html'
     queryset = ContactUs.objects.all()
 
 
 class ContactUsDetailView(DetailView):
-    template_name = "contact_us/contact_us_details.html"
+    template_name = 'contact_us/contact_us_details.html'
     queryset = ContactUs.objects.all()
 
 
 class ContactUsUpdateView(UpdateView):
     form_class = ContactUsForm
-    template_name = "contact_us/contact_us_update.html"
+    template_name = 'contact_us/contact_us_update.html'
     queryset = ContactUs.objects.all()
-    success_url = "/contact_us/list/"
+    success_url = reverse_lazy('currency:contactus-list')
 
 
 class ContactUsDeleteView(DeleteView):
-    template_name = "contact_us/contact_us_delete.html"
+    template_name = 'contact_us/contact_us_delete.html'
     queryset = ContactUs.objects.all()
-    success_url = "/contact_us/list/"
+    success_url = reverse_lazy('currency:contactus-list')
 
 
 class SourceCreateView(CreateView):
     form_class = SourceForm
-    template_name = "source/source_create.html"
+    template_name = 'source/source_create.html'
     queryset = Source.objects.all()
-    success_url = "/source/list/"
+    success_url = reverse_lazy('currency:source-list')
 
 
 class SourceListView(ListView):
-    template_name = "source/source_list.html"
+    template_name = 'source/source_list.html'
     queryset = Source.objects.all()
 
 
 class SourceDetailView(DetailView):
-    template_name = "source/source_details.html"
+    template_name = 'source/source_details.html'
     queryset = Source.objects.all()
 
 
 class SourceUpdateView(UpdateView):
     form_class = SourceForm
-    template_name = "source/source_update.html"
+    template_name = 'source/source_update.html'
     queryset = Source.objects.all()
-    success_url = "/source/list/"
+    success_url = reverse_lazy('currency:source-list')
 
 
 class SourceDeleteView(DeleteView):
-    template_name = "source/source_delete.html"
+    template_name = 'source/source_delete.html'
     queryset = Source.objects.all()
-    success_url = "/source/list/"
+    success_url = reverse_lazy('currency:source-list')
--- app/settings/urls.py
+++ app/settings/urls.py
@@ -1,39 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from currency.views import MainPageView
 
-from currency.views import (
-    index,
-
-    RateListView, RateDetailView, RateUpdateView, RateCreateView, RateDeleteView,
-
-    ContactUsCreateView, ContactUsListView, ContactUsDetailView, ContactUsUpdateView,
-    ContactUsDeleteView,
-
-    SourceCreateView, SourceListView, SourceDetailView, SourceUpdateView, SourceDeleteView
-)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+    path('', MainPageView.as_view()),
+
     path('__debug__/', include('debug_toolbar.urls')),
 
-    path('', index),
-
-    path('rate/create/', RateCreateView.as_view()),
-    path('rate/list/', RateListView.as_view()),
-    path('rate/details/<int:pk>/', RateDetailView.as_view()),
-    path('rate/update/<int:pk>/', RateUpdateView.as_view()),
-    path('rate/delete/<int:pk>/', RateDeleteView.as_view()),
-
-    path('contact_us/create/', ContactUsCreateView.as_view()),
-    path('contact_us/list/', ContactUsListView.as_view()),
-    path('contact_us/details/<int:pk>/', ContactUsDetailView.as_view()),
-    path('contact_us/update/<int:pk>/', ContactUsUpdateView.as_view()),
-    path('contact_us/delete/<int:pk>/', ContactUsDeleteView.as_view()),
-
-    path('source/create/', SourceCreateView.as_view()),
-    path('source/list/', SourceListView.as_view()),
-    path('source/details/<int:pk>/', SourceDetailView.as_view()),
-    path('source/update/<int:pk>/', SourceUpdateView.as_view()),
-    path('source/delete/<int:pk>/', SourceDeleteView.as_view()),
+    path('currency/', include('currency.urls'))
 ]
