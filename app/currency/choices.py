32d3640a0cd6512428135da5490e7c9b0fa6b2b7
Martyniuk Vadym <martynyukvadum@gmail.com>
2023-04-09 22:59:11 +0300

    Added new foreign key to Rate model
    Created new model CurrencyInfo
    Added custom django-admin command to populate CurrencyInfo DB table
    Changed choices for currency app, as well as Rate model

--- app/currency/choices.py
+++ app/currency/choices.py
@@ -1,11 +1,20 @@
 from django.db import models
 
 
-class RateCurrencyChoices(models.IntegerChoices):
+class CurrencyInfoCurrencyChoices(models.IntegerChoices):
     USD = 1, 'Dollar'
     EUR = 2, 'Euro'
     GBP = 3, 'Pound Sterling'
     JPY = 4, 'Yen'
+
+
+CurrencyInfoCurrencyMap = {
+    CurrencyInfoCurrencyChoices.USD: ('Dollar', 'url1'),
+    CurrencyInfoCurrencyChoices.EUR: ('Euro', 'url2'),
+    CurrencyInfoCurrencyChoices.UAH: ('Hryvnia', 'url3'),
+    CurrencyInfoCurrencyChoices.GBP: ('Pound Sterling', 'url]4'),
+    CurrencyInfoCurrencyChoices.JPY: ('Yen', 'url5'),
+}
 
 
 class RequestMethodChoices(models.IntegerChoices):
