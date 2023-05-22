66e5f8ad1123c79c59a713cf897e67b13b16bde3
Vadym Martyniuk <MartynyukVadum@gmail.com>
2023-05-22 22:19:47 +0300

    Added nginx and gunicorn support
    Provided nginx configuration files in nginx dir
    Note that nginx must be installed separately
    Note that to make static work you need to collect it using collect static, as well as to change dir in default config file of nginx

--- app/settings/settings.py
+++ app/settings/settings.py
@@ -138,6 +138,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR.parent / 'static_content' / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
